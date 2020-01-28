038a385a90cf2baa952ae6ba48f94c5704d1acb4
Will <wfoster12@gmail.com>
2020-01-28 09:08:10 +0000

    Initial layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TypingGame';
+  onType(text){
+    const randomText = document.querySelector('.randomText').innerHTML;
+    const index = text.length -1;
+  }
 }
